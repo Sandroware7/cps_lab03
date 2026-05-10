41e5f93489c280c4f72262009862046d4d6bedda
Sandroware7 <sandro.rodriguez.b@tecsup.edu.pe>
2026-05-10 10:12:11 -0500

    ajuste

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentcontrollerS2.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentcontrollerS2.java
@@ -1,4 +1,7 @@
 package pe.edu.tecsup.lab03.controllers;
 
 public class StudentcontrollerS2 {
+    public void showInfo() {
+        System.out.println("Este es el primer cambio al archivo StudentController");
+    }
 }
