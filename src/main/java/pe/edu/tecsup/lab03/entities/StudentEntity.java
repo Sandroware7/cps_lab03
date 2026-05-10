26a3e5c146264185cd8d408d0b86c5cb3d207d12
Jorge <jorgemerino97@gmail.com>
2026-05-10 09:40:03 -0500

    Modificación de StudentEntity para el sprint-2

--- src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
+++ src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
@@ -5,4 +5,16 @@ public class StudentEntity {
     private String nombre;
     private String programa;
 
+    // Actualización de branch 2, Constructor
+    public StudentEntity(Long id, String nombre, String programa) {
+        this.id = id;
+        this.nombre = nombre;
+        this.programa = programa;
+    }
+
+    // Método simple
+    @Override
+    public String toString() {
+        return "Student: " + nombre;
+    }
 }
